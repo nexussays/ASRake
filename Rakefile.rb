bd29eac834f9c04d1b156fa24a9167f2368a9081
Malachi Griffie <malachi@nexussays.com>
2012-06-07 02:53:39 -0700

    updated test Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,13 +2,16 @@ $: << 'lib'
 
 require './lib/asrake'
 
-task :default => 'swc:build'
+task :default => :build
 
 FlexSDK::SDK_PATHS << "C:/develop/sdk/flex_sdk_4.6.0.23201"
 #FlexSDK::SDK_PATHS << 'C:\develop\sdk\flex_sdk_4.6.0.23201'
+#FlexSDK::SDK_PATHS << 'C:\bad\develop\sdk\flex_sdk_4.6.0.23201'
 
-ASRake::SWC.new do |t|
-	t.source_path = 'C:\Users\nexus\Development\Projects\Personal\nexuslib\code\projects\reflection\src'
-	t.output = 'C:\Users\nexus\Development\Projects\Personal\nexuslib\code\projects\reflection\bin\test.swc\\'
-	t.library_path << 'C:\Users\nexus\Development\Projects\Personal\nexuslib\code\lib\blooddy_crypto_0.3.5\blooddy_crypto.swc'
+desc "The default build task"
+ASRake::SWC.new :build do |build|
+	build.target_player = 11.1
+	build.source_path << '..\nexuslib\code\projects\reflection\src'
+	build.output = '..\nexuslib\code\projects\reflection\bin\test.swc'
+	build.statically_link_only_referenced_classes << '..\nexuslib\code\lib\blooddy_crypto_0.3.5\blooddy_crypto.swc'
 end
