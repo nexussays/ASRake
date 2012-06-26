918083c97681ed3c4e3b58e0de26bbecc1ba45c5
Malachi Griffie <malachi@nexussays.com>
2012-06-26 03:07:33 -0700

    Improved clean task to take multiple args

--- lib/asrake/clean_task.rb
+++ lib/asrake/clean_task.rb
@@ -3,15 +3,30 @@ require 'rake/tasklib'
 module ASRake
 class CleanTask < Rake::TaskLib
 
-	def initialize(args)
+	attr_accessor :clean_list
+	attr_accessor :clobber_list
+
+	def initialize(*args)
+
+		self.clean_list = FileList.new
+		self.clobber_list = FileList.new
+
+		args.each do |configArgs|
+			clean_list.include(File.join(configArgs.output_dir, "*"))
+			clean_list.exclude(configArgs.output)
+
+			clobber_list.include(File.join(configArgs.output_dir, "*"))
+		end
+
 		desc "Remove package results & build artifacts"
 		task :clean do
-			FileList.new(File.join(args.output_dir, "*")).exclude(args.output).each { |f| rm_r f rescue nil }
+			clean_list.each { |f| rm_r f rescue nil }
 		end
 
 		desc "Remove all build & package results"
+		# adding clean as a dependency in case anything is added to the clean task later
 		task :clobber => [:clean] do
-			FileList.new(File.join(args.output_dir, "*")).each { |f| rm_r f rescue nil }
+			clobber_list.each { |f| rm_r f rescue nil }
 		end
 	end
 
