a4c39b1e341d45c56f21587c4051f6a8493ded5c
Malachi Griffie <malachi@nexussays.com>
2012-09-19 00:58:37 -0700

    Improved functionality and fixed bugs in asdoc
    * Added remaining arguments to Asdoc
    * Added add() method which takes any instance of compiler args and adds the necessary fields to the asdoc output
    * Not displaying command output to stdout

--- lib/asrake/asdoc.rb
+++ lib/asrake/asdoc.rb
@@ -6,25 +6,19 @@ require 'nokogiri'
 module ASRake
 class Asdoc
 
-	attr_accessor :output
-
-	attr_accessor :doc_sources
-	attr_accessor :doc_classes
-	attr_accessor :doc_namespaces
-	attr_accessor :source_path
-	attr_accessor :library_path
-
-	attr_accessor :load_config
-	#
-	# The path to the ASDoc template directory. The default is the asdoc/templates directory in the ASDoc
-	# installation directory. This directory contains all the HTML, CSS, XSL, and image files used for
-	# generating the output. 
-	#
-	attr_accessor :templates_path
-
 	attr_accessor :additional_args
 
+	# we have some special handling of this
+	attr_accessor :doc_sources
+
 	@@compiler_args = [
+		[:source_path, :dirs],
+		[:load_config, :dirs],
+		[:library_path, :dirs],
+		[:namespace, :string],
+		#
+		# The output directory for the generated documentation. The default value is "asdoc-output".
+		#
 		[:output, :dir],
 		#
 		# When true, retain the intermediate XML files created by the ASDoc tool. The default value is false. 
@@ -45,11 +39,73 @@ class Asdoc
 		# All errors are written to the validation_errors.log file. 
 		#
 		[:lenient, :bool],
-		[:source_path, :array],
-		[:load_config, :array],
-		[:library_path, :array],
+		#
+		# The path to the ASDoc template directory. The default is the asdoc/templates directory in the ASDoc
+		# installation directory. This directory contains all the HTML, CSS, XSL, and image files used for
+		# generating the output. 
+		#
+		[:templates_path, :dir],
+		#
+		# A list of classes to document. These classes must be in the source path. This is the default option.
+		# This option works the same way as does the -include-classes option for the compc component compiler.
+		#
 		[:doc_classes, :array],
+		# 
+		# A list of classes not documented. You must specify individual class names.
+		# Alternatively, if the ASDoc comment for the class contains the @private tag, is not documented. 
+		#
+		[:exclude_classes, :array],
+		#
+		# A list of URIs to document. The classes must be in the source path.
+		# You must include a URI and the location of the manifest file that defines the contents of this namespace.
+		# This option works the same way as does the -include-namespaces option for the compc component compiler.
+		#
 		[:doc_namespaces, :array],
+		#
+		# Specifies the location of the include examples used by the @includeExample tag. This option specifies the
+		# root directory. The examples must be located under this directory in subdirectories that correspond to the
+		# package name of the class. For example, you specify the examples-path as c:\myExamples. For a class in the
+		# package myComp.myClass, the example must be in the directory c:\myExamples\myComp.myClass.
+		#
+		[:examples_path, :dir],
+		#
+		# The text that appears at the bottom of the HTML pages in the output documentation.
+		#
+		[:footer, :string],
+		#
+		# The text that appears in the browser window in the output documentation.
+		# The default value is "API Documentation".
+		#
+		[:window_title, :string],
+		#
+		# An integer that changes the width of the left frameset of the documentation. You can change this
+		# size to accommodate the length of your package names.
+		# The default value is 210 pixels.
+		#
+		[:left_frameset_width, :string],
+		#
+		# The text that appears at the top of the HTML pages in the output documentation.
+		# The default value is "API Documentation".
+		#
+		[:main_title, :string],
+		#
+		# The descriptions to use when describing a package in the documentation.
+		# You can specify more than one package option.
+		# The following example adds two package descriptions to the output:
+		# asdoc = ASRake::Asdoc.new
+		# asdoc.package << 'com.my.business "Contains business classes and interfaces"'
+		# asdoc.package << 'com.my.commands "Contains command base classes and interfaces"'
+		[:package, :array],
+		#
+		# Specifies an XML file containing the package descriptions. 
+		#
+		[:package_description_file, :dir],
+		#
+		# Disable strict compilation mode. By default, classes that do not define constructors, or contain methods
+		# that do not define return values cause compiler failures. If necessary, set strict to false to override
+		# this default and continue compilation.
+		#
+		[:strict, :bool]
 	]
 
 	@@compiler_args.each do |name, type|
@@ -57,19 +113,25 @@ class Asdoc
 	end
 
 	def initialize
-
 		# set all defaults
 		@@compiler_args.each do |name, type|
-			instance_variable_set("@#{name}", []) if type == :array
+			instance_variable_set("@#{name}", []) if type == :array || type == :dirs
 		end
 
 		@doc_sources = []
 
 		yield self if block_given?
-		
 	end
 
-	def execute
+	def add(args)
+		self.source_path << args.source_path
+		self.library_path << args.library_path
+		self.library_path << args.include_libraries
+		self.library_path << args.external_library_path
+		args.source_path.each { |p| self.doc_classes << ASRake::get_classes(p) } if args.kind_of? CompcArguments
+	end
+
+	def execute(&block)
 		command = "#{FlexSDK::asdoc}"
 
 		@@compiler_args.each do |name, type|
@@ -78,13 +140,19 @@ class Asdoc
 			case type
 			when :bool
 				command << " -#{arg}=#{value}" if value
-			when :array
+			when :dirs
 				value.flatten!
 				value.uniq!
-				value = value.map{|s| s.index(' ') != nil ? "'#{s}'" : s} if value.length > 1
+				value = value.map{|s| s.index(' ') != nil ? "\"#{s}\"" : s} if value.length > 1
 				command << " -#{arg} #{cf value.join(' ')}" if !value.empty?
 			when :dir
 				command << " -#{arg}=#{cf value}" if value != nil
+			when :array
+				value.flatten!
+				value.uniq!
+				command << " -#{arg} #{value.join(' ')}" if !value.empty?
+			when :string
+				command << " -#{arg} #{value}" if value != nil
 			else
 				fail "unknown type #{type}"
 			end
@@ -100,7 +168,8 @@ class Asdoc
 
 		command << " #{additional_args}" if self.additional_args != nil
 		
-		run command
+		puts if !block_given?
+		run(command, true, &block)
 	end
 
 end
