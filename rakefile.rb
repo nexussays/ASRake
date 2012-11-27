c7ef614373fefdf2dd90210f333631f59d5024de
Malachi Griffie <malachi@nexussays.com>
2012-11-27 02:03:17 -0800

    Switched build to use bundler gem tasks

--- rakefile.rb
+++ rakefile.rb
@@ -1,27 +1,10 @@
-require 'rubygems'
-require 'rubygems/package_task'
 require 'bundler/gem_tasks'
 
 task :default do
 	system "rake --tasks"
 end
 
-package = Gem::PackageTask.new Gem::Specification.load('asrake.gemspec') do |pkg|
-	pkg.need_zip = false
-end
-
-desc "Push gem to rubygems.org"
-task :deploy => :package do
-	sh "gem push #{package.package_dir}/#{package.name}.gem"
-end
-
-desc "Package & install the gem locally"
-task :test => [:uninstall, :package] do
-	sh "gem install #{package.package_dir}/#{package.name}.gem"
-end
-
-# Save a few key strokes
-desc "Uninstall asrake gem"
-task :uninstall do
-	sh 'gem uninstall asrake'
+desc "Remove packaged gem"
+task :clobber do
+	rm_rf 'pkg'
 end
