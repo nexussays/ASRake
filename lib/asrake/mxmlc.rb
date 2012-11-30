6e11e92f8b332432428e898c13a54c60b2d23c1c
Malachi Griffie <malachi@nexussays.com>
2012-11-30 05:51:18 -0800

    Added file_specs arg and error code handling to mxmlc

--- lib/asrake/mxmlc.rb
+++ lib/asrake/mxmlc.rb
@@ -6,9 +6,31 @@ module ASRake
 class Mxmlc < BaseCompiler
 
 	include Rake::DSL
+
+	attr_accessor :file_specs
 	
 	def initialize(swf_file)
 		super(swf_file, FlexSDK::mxmlc)
+	end
+
+	def generate_args
+		mxmlc = super
+		
+		mxmlc << " -file-specs=#{file_specs}" if file_specs != nil
+
+		return mxmlc
+	end
+
+	protected
+
+	def handle_execute_error code
+		case code
+		when 1
+			raise "A target file can be specified by setting file_specs to a valid .as or .mxml file.\n" +
+				"eg:\n" +
+				"  swf = ASRake::Mxmlc.new #{output}\n" +
+				"  swf.file_specs = 'src/Main.as'" if file_specs == nil
+		end
 	end
 
 end
